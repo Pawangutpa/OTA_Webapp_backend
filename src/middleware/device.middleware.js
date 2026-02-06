c1eb1fb43a129bf1925c11d413a19ad9dc26d074
dsjchd <pawan246g@gmail.com>
2026-02-06 23:51:51 +0530

    upeercase in mac of device

--- src/middleware/device.middleware.js
+++ src/middleware/device.middleware.js
@@ -1,11 +1,17 @@
 const Device = require("../models/device.model");
 
 module.exports = async (req, res, next) => {
-  const device = await Device.findById(req.params.deviceId);
-  if (!device) return res.status(404).json({ message: "Device not found" });
+  const device = await Device.findOne({
+    deviceId: req.params.deviceId.toUpperCase()
+  });
 
-  if (device.blocked)
+  if (!device) {
+    return res.status(404).json({ message: "Device not found" });
+  }
+
+  if (device.blocked) {
     return res.status(403).json({ message: "Device is blocked" });
+  }
 
   req.device = device;
   next();
